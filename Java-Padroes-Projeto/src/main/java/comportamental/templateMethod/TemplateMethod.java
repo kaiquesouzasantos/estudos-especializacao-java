2fd5dbd699d2b4dabe836ce3855f02f1176cd900
Kaique Souza Santos <92165719+kaiquesouzasantos@users.noreply.github.com>
2024-01-05 20:35:01 -0300

    Update TemplateMethod.java

--- Java-Padroes-Projeto/src/main/java/comportamental/templateMethod/TemplateMethod.java
+++ Java-Padroes-Projeto/src/main/java/comportamental/templateMethod/TemplateMethod.java
@@ -17,7 +17,8 @@ public class TemplateMethod {
         - Implementações do padrão Template Method tendem a ser mais difíceis de se manter quanto mais etapas eles tiverem.
 
     SOLUCAO:
-
+        - O Template Method é um padrão de projeto comportamental que permite definir o esqueleto de um algoritmo em uma classe base e 
+          permitir que as subclasses substituam as etapas sem alterar a estrutura geral do algoritmo.
     */
 
     public static void main(String[] args) {
